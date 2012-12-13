5a37811fd55724d7334b587e51e471f96a454ced
diegodayan <39ab1dbb984021b5b60c89011fd4123bb520de51@server>
2012-12-12 19:45:31 -0800

    Refactoring

---
+++ api/src/test/java/org/openmrs/module/dss/test/DummyTest.java
@@ -0,0 +1,32 @@
+package org.openmrs.module.dss.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Diego Dayan
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({})
+public class DummyTest {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
